37bc485b6ba34f7a034db094a7cc7d8a60870dd8
Sheogorattio <email>
2023-10-18 06:22:18 +0300

    Add project files.

---
+++ Puzzle/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Puzzle
+{
+    public partial class MainWindow : Window
+    {
+        public static Image global_image;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Image_Drop(object sender, DragEventArgs e)
+        {
+            ((Image)sender).Source = global_image.Source;
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            global_image = (Image)sender;
+            DragDrop.DoDragDrop(global_image, global_image.Source, DragDropEffects.Copy);
+        }
+    }
+}
